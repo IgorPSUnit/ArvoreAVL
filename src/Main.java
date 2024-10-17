1cf1233a5a5eb7f859899769403e8e9ebbe3cc52
IgorPSUnit <igor.profeta@souunit.com.br>
2024-10-17 18:38:44 -0300

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        ArvoreAVL arvoreBB = new ArvoreAVL();
+        arvoreBB.inserir(12);
+        arvoreBB.inserir(4);
+        arvoreBB.inserir(15);
+        arvoreBB.inserir(3);
+        arvoreBB.inserir(9);
+        arvoreBB.inserir(14);
+        arvoreBB.inserir(18);
+        arvoreBB.inserir(8);
+        arvoreBB.inserir(10);
+        arvoreBB.inserir(16);
+        arvoreBB.inserir(20);
+        arvoreBB.inserir(6);
+        arvoreBB.inserir(17);
+        arvoreBB.inserir(5);
+        arvoreBB.inserir(7);
+        arvoreBB.preorder();
+        arvoreBB.inorder();
+        arvoreBB.postorder();
+        arvoreBB.fatorBalanceamento();
+        arvoreBB.imprimirArvore();
+
+    }
+}
---
+++ src/No.java
@@ -0,0 +1,50 @@
+public class No {
+    private int valor;
+    private No left;
+    private No right;
+    private int fb;
+
+    public No(int valor) {
+        this.valor = valor;
+        this.left = null;
+        this.fb = this.fb;
+        this.right = null;
+    }
+
+    public int getValor() {
+        return this.valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public No getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(No left) {
+        this.left = left;
+    }
+
+    public No getRight() {
+        return this.right;
+    }
+
+    public void setRight(No right) {
+        this.right = right;
+    }
+
+    public String toString() {
+        int var10000 = this.valor;
+        return "No{valor=" + var10000 + ", left=" + String.valueOf(this.left) + ", right=" + String.valueOf(this.right) + ", fb=" + this.fb + "}";
+    }
+
+    public int getFb() {
+        return this.fb;
+    }
+
+    public void setFb(int fb) {
+        this.fb = fb;
+    }
+}
